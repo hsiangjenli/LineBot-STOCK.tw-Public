c8476fd6836dc164e7164d197c569f9d220677e3
hsiangjenli <71996166+hsiangjenli@users.noreply.github.com>
2020-10-18 19:53:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,11 @@ Created on Thu Oct  1 10:42:26 2020
 """
 
 from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
+
+###import 副程式
 import candlestickchart
 import regex as re
 
@@ -44,15 +42,18 @@ def callback():
 ###傳送訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = str(event.message.text).upper().strip() ###收到的訊息
+    msg = str(event.message.text).upper().strip() # 使用者輸入的內容
     profile = line_bot_api.get_profile(event.source.user_id)
     user_name = profile.display_name #使用者名稱
     user_id = profile.user_id # 發訊者ID
-    
-    #發送 K線圖
     if re.match('C', msg):
         imgur_url = candlestickchart.plot_candlestick(msg[1:15], 90, CLIENT_ID, savefig)
-        line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=imgur_url, preview_image_url=imgur_url))
+        line_bot_api.push_message(user_id, [TextSendMessage(text='正在畫圖，請稍後....'), ImageSendMessage(original_content_url=imgur_url, preview_image_url=imgur_url)])
+        return 0
+    elif re.match('M', msg):
+        imgur_url = candlestickchart.plot_candlestick_macd(msg[1:20], 90, CLIENT_ID, savefig)
+        line_bot_api.push_message(user_id, [TextSendMessage(text='正在畫圖，請稍後....'), ImageSendMessage(original_content_url=imgur_url, preview_image_url=imgur_url)])
+        return 0
         
 #主程式
 import os
